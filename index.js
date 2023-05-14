de63721a5e0d4d6d636080ff06a7bce5365c03e7
Heva <demo.south.1907@gmail.com>
2023-05-14 17:28:53 +0700

    add code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const { Aki } = require('aki-api')
+const NodeCache = require( "node-cache" )
 
 const app = express()
-app.use(express.json());
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.send('Express JS on Vercel')
@@ -10,95 +12,6 @@ app.get('/', (req, res) => {
 app.get('/ping', (req, res) => {
     res.send('pong 🏓')
 })
-
-const region_default = 'en';
-// const region_default = 'en_animals';
-// const region_default = 'en_objects';
-const list_region = [
-  'en',
-  'en_objects',
-  'en_animals',
-  'ar',
-  'cn',
-  'de',
-  'de_animals',
-  'es',
-  'es_animals',
-  'fr',
-  'fr_objects',
-  'fr_animals',
-  'il',
-  'it',
-  'it_animals',
-  'jp',
-  'jp_animals',
-  'kr',
-  'nl',
-  'pl',
-  'pt',
-  'ru',
-  'tr',
-  'id'
-];
-
-const childMode = false;
-const proxy = undefined;
-const myCache = new NodeCache();
-
-app.post('/start', async (req, res) => {
-	console.log(req.body)
-	let user_id = req.body.user_id;
-	let region = req.body.region;
-	if (!region) {
-		region = region_default
-	}
-
-	if (list_region.includes(region)) {
-		try {
-			const aki = new Aki({ region, childMode, proxy });
-			await aki.start();
-			myCache.set( user_id, aki, 10000 );
-			res.json(aki);
-		}
-		catch(err) {
-			console.log("loi roi")
-			res.json({});
-		}
-	} else {
-		res.json({});
-	}
-	
-});
-
-app.post('/get-by-id', async (req, res) => {
-	console.log(req.body)
-	let user_id = req.body.user_id;
-
-	let aki = myCache.get( user_id );
-	res.json(aki);
-});
-
-app.post('/answer', async (req, res) => {
-	console.log(req.body)
-	let user_id = req.body.user_id;
-	let answer = req.body.answer;
-
-	let aki = myCache.get( user_id );
-
-	if (aki) {
-		await aki.step(answer);
-
-		if (aki.progress >= 70 || aki.currentStep >= 78) {
-			// winnnnnn
-			await aki.win();
-		}
-		myCache.set( user_id, aki, 10000 );
-	}
-
-	
-	res.json(aki);
-});
-
 
 const port = process.env.PORT || 8080
 
